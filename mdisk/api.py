f8bbac30e83c8e722e2f5c7f544d6d8725ebd2f5
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-08-16 08:09:31 +0530

    Update api.py

--- mdisk/api.py
+++ mdisk/api.py
@@ -28,7 +28,7 @@ class Mdisk:
         self.api_key = api_key
         self.base_url = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
   
-    def upload(self, link:str, silently_fail:bool = False) -> str:
+    def upload(self, link:str):
         '''
         Upload files using direct links
         
@@ -42,26 +42,9 @@ class Mdisk:
             #response = r.json()
             #data = dict(response)
             #mdisk = data["sharelink"]
-            return await self.__error_handler(url=link, silently_fail=silently_fail, exception=LinkInvalid)
+            return mdisk
 
         except ConnectionError as e:
             sys.exit(f"ERROR : {e}")
 
-    def __error_handler(self, url:str, silently_fail:bool, exception=Exception, message="Some error occurred during converting: %s"):
-        """
-        If the URL is valid, return it. If it's not, return it or raise an exception, depending on the value
-        of the `silently_fail` parameter
-        
-        :param url: The URL to be validated
-        :type url: str
-        :param silently_fail: If True, then if the URL is not valid, return the original URL. If False,
-        raise an exception
-        :type silently_fail: bool
-        :param exception: The exception to raise if the URL is not valid
-        :return: The url is being returned.
-        """
-        if silently_fail:
-            return url
-        else:
-            raise exception(message % url)
-
+    
